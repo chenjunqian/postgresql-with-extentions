f901c48880baf6eeaa62462bd644eafce1bf0eb5
Chenjunqian <chenjunqian0810@foxmail.com>
2025-03-13 01:06:42 +0800

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,41 @@
+CREATE EXTENSION vector;
+CREATE EXTENSION pgroonga;
+
+CREATE TABLE job_detail (
+    id varchar(64) NOT NULL,
+    title varchar(128),
+    job_desc text,
+    job_tags text [],
+    link varchar(128),
+    "source" varchar(64),
+    location varchar(255),
+    salary varchar(255),
+    update_time date,
+    job_desc_embedding vector,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX pgroonga_idx_job_desc ON job_detail USING pgroonga ("job_desc");
+
+ALTER TABLE job_detail ADD CONSTRAINT unique_link UNIQUE (link);
+
+CREATE TABLE user_info (
+    id varchar(64) NOT NULL,
+    name varchar(64),
+    email varchar(64),
+    telegram_id varchar(64),
+    resume_embedding vector,
+    resume text,
+    job_expectations text,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE user_matched_job (
+    user_id varchar(64) NOT NULL,
+    job_id varchar(64) NOT NULL,
+    update_time timestamp without time zone,
+    notification boolean DEFAULT false,
+    match_score varchar,
+    match_reason text,
+    PRIMARY KEY (user_id, job_id)
+);
